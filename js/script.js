1e9e437fa817de832fd3a2ec6e0d0a39ac2c5e99
github username <github email address>
2017-07-20 13:48:23 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  
+
   // browser refreshes on submit
   $('form').on('submit', function(){
     var name = $('#name').val();
@@ -7,12 +7,12 @@ $(document).ready(function(){
   });
 
   //examine event object
-  // $('form').on('submit', function(event){
-  //   var name = $('#name').val();
-  //   $("#hello").text("Hello, " + name);
-  //   debugger;
-  //   event.preventDefault();
-  // });
+  $('form').on('submit', function(event){
+    var name = $('#name').val();
+    $("#hello").text("Hello, " + name);
+    debugger;
+    event.preventDefault();
+  });
 
   // stop page refresh
   // $('form').on('submit', function(event){
